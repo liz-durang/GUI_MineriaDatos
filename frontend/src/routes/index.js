4c9edc382fb13557b29ffd4ec851d374735d2059
liiz-durang <lizeth.durang@hotmail.com>
2023-05-23 16:11:33 -0600

    Rou†er and Layout check

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,19 @@
+
+export const routesApp = [
+    {
+      to: '/',
+      text: 'Home'
+    },
+    {
+      to: '/eda',
+      text: '1. Análisis Exploratorio de Datos'
+    },
+    {
+      to: '/pca',
+      text: '2. Análisis de Componentes Principales'
+    },
+    {
+    to: '/bosques',
+    text: '3. Bosques Aleatorios'
+    },
+  ];
