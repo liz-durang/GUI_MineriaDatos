445f33a07fef0c514fe56c7b959cde4969211b0c
liiz-durang <lizeth.durang@hotmail.com>
2023-05-24 06:43:51 -0600

    solucion a problema por CORS

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes.pca.routes import router as pca_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins='http://localhost:3000/',
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(pca_router, prefix="/pca", tags=["PCA"])
 
